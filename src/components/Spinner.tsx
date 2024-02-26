20ed163a7abb3ed1692125de5ebb895d3c9dabcd
pero020 <petar.przic1@gmail.com>
2024-02-26 19:21:24 +0100

    Add github pages, add z index spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,5 @@
 const Spinner = () => (
-  <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center  z-50">
+  <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-30">
     <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-200"></div>
   </div>
 );
